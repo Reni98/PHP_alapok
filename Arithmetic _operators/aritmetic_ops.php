0c204452f0f089aa5d3be42ec6675fb6d2495f4f
Reni98 <93825160+Reni98@users.noreply.github.com>
2023-09-09 12:05:27 +0200

    Add files via upload

---
+++ Arithmetic _operators/aritmetic_ops.php
@@ -0,0 +1,28 @@
+<?
+//https://www.w3schools.com/php/php_operators.asp
+//Arithmetic operators - Aritmetikai operátorok
+/*
++
+-
+*
+/
+%
+**
+ */
+
+ //echo 25 + 45 . PHP_EOL;
+ //echo 25 - 45 . PHP_EOL;
+ //echo 25 * 45 . PHP_EOL;
+ //echo 25 / 45 . PHP_EOL;
+ //echo 45 % 25 . PHP_EOL;
+ //echo 2 ** 3 . PHP_EOL;
+ 
+//Arithmetic operator presedence - Aritmetikai operátor elsőbbség
+// () ** * / % + -
+//echo 20 + 2 * 4 . PHP_EOL;
+// echo 3 / 2 * 4 . PHP_EOL;
+// echo (20 + 2) *4 . PHP_EOL;
+//echo 4 * 2 ** 2 . PHP_EOL;
+// echo (2 * 2) ** 2 . PHP_EOL;
+// echo 20 % 2 *4 . PHP_EOL;
+// echo 2 / 4 * 4 . PHP_EOL;
