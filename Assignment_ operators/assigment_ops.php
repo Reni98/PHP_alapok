75e6980eef906d349bccf1905688f32b42df9dde
Reni98 <93825160+Reni98@users.noreply.github.com>
2023-09-09 12:22:41 +0200

    Add files via upload

---
+++ Assignment_ operators/assigment_ops.php
@@ -0,0 +1,19 @@
+<?
+//Assignment operators - Hozzárendelési (Értékadó) operátorok
+/*
+
+=
++=
+-=
+/=
+%=
+
+*/
+$szam = 20; 
+//$szam += 4;
+// $szam -= 4;
+// $szam *= 4;
+// $szam /= 4;
+// $szam %= 4;
+
+echo $szam . PHP_EOL;
